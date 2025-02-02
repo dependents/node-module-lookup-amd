8f275cf7ef2a4f4a5d7e7ed65767b2faa6dfc28c
XhmikosR <xhmikosr@gmail.com>
2025-02-02 10:01:27 +0200

    Tweak cli

--- bin/cli.js
+++ bin/cli.js
@@ -14,7 +14,12 @@ program
   .option('-c, --config <path>', 'location of a RequireJS config file for AMD')
   .option('-f, --filename <path>', 'file containing the dependency')
   .option('-d, --directory <path>', 'directory containing all files')
+  .showHelpAfterError()
   .parse();
+
+if (program.args.length === 0) {
+  program.help();
+}
 
 const partial = program.args[0];
 const { filename, config } = program.opts();
