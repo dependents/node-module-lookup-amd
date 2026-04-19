6de35d9ec2ff63c2baaad89c99a417e0f664a7cc
XhmikosR <xhmikosr@gmail.com>
2026-04-19 17:41:10 +0300

    Require CLI path argument (#60)

--- bin/cli.js
+++ bin/cli.js
@@ -10,16 +10,12 @@ program
   .name(name)
   .description(description)
   .version(version)
-  .usage('[options] <path>')
+  .argument('<path>', 'the partial/dependency path to resolve')
   .option('-c, --config <path>', 'location of a RequireJS config file for AMD')
   .option('-f, --filename <path>', 'file containing the dependency')
   .option('-d, --directory <path>', 'directory containing all files')
   .showHelpAfterError()
   .parse();
-
-if (program.args.length === 0) {
-  program.help();
-}
 
 const partial = program.args[0];
 const { filename, config } = program.opts();
